762f02b3e283a8099da1e6390278deaa70bfaf90
TechieGuy12 <psalmon12@gmail.com>
2021-06-02 14:57:27 -0400

    Added folder arg

--- FileVerification/Program.cs
+++ FileVerification/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace FileVerification
 {
@@ -9,16 +11,32 @@ namespace FileVerification
     {
         public static int NumFolders { get; set; }
 
-        static void Main(string[] args)
-        {            
+        static int Main(string[] args)
+        {
+            RootCommand rootCommand = new RootCommand(
+              description: "Generates the hash of all files in a folder tree and stores the hashes in text files in each folder.");
+
+            var folderOption = new Option<string>(
+                    aliases: new string[] { "--folder", "-f" }, 
+                    description: "The folder containing the files to verify with a hash."
+                );
+            folderOption.IsRequired = true;
+            rootCommand.AddOption(folderOption);
+            rootCommand.Handler = CommandHandler.Create<string>(Verify);
+            return rootCommand.Invoke(args);
+        }
+
+        static void Verify(string folder)
+        {
             FileSystemCrawlerSO fsc = new FileSystemCrawlerSO();
             Stopwatch watch = new Stopwatch();
 
-            string docPath = @"F:\HashTest";
+            //string docPath = @"F:\HashTest";
             watch.Start();
-            fsc.CollectFolders(docPath);
+            fsc.CollectFolders(folder);
             fsc.CollectFiles();
             watch.Stop();
+
             Logger.WriteLine("--------------------------------------------------------------------------------");
             Logger.WriteLine($"Folders:   {fsc.NumFolders}");
             Logger.WriteLine($"Files:     {fsc.NumFiles}");
