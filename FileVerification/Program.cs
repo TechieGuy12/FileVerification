d0990a971c96791ed6b7512d0b4b56ca96b3b2f4
TechieGuy12 <psalmon12@gmail.com>
2022-07-29 21:18:44 -0400

    Restructure program entry

--- FileVerification/Program.cs
+++ FileVerification/Program.cs
@@ -28,6 +28,15 @@ namespace TE.FileVerification
 
         public static int NumFolders { get; set; }
 
+        /// <summary>
+        /// The main entry into the application.
+        /// </summary>
+        /// <param name="args">
+        /// The arguments passed into the application.
+        /// </param>
+        /// <returns>
+        /// 0 on success, otherwise a non-zero value.
+        /// </returns>
         static int Main(string[] args)
         {
             RootCommand rootCommand = new RootCommand(
@@ -146,14 +155,14 @@ namespace TE.FileVerification
                     checksumFile = ChecksumFile.DEFAULTCHECKSUMFILENAME;
                 }
 
-                if (excludeSubDir == null)
-                {
-                    excludeSubDir = false;
-                }
-
                 if (algorithm == null)
                 {
                     algorithm = HashAlgorithm.SHA256;
+                }
+
+                if (excludeSubDir == null)
+                {
+                    excludeSubDir = false;
                 }
 
                 if (threads == null || threads == default(int))
@@ -176,99 +185,166 @@ namespace TE.FileVerification
                 // generate and verify the hashes of the files
                 if (string.IsNullOrWhiteSpace(hashOption) && !hashOnlyOption)
                 {
-                    // Read the settings file if one was provided as an argument
-                    Settings? settings = null;
-                    Exclusions? exclusions = null;
-                    if (!string.IsNullOrWhiteSpace(settingsFile))
-                    {
-                        ISettingsFile xmlFile = new XmlFile(settingsFile);
-                        settings = xmlFile.Read();
-                        if (settings != null)
-                        {
-                            exclusions = settings.Exclusions;
-                        }
-                    }
-
-                    Logger.WriteLine("--------------------------------------------------------------------------------");
-                    Logger.WriteLine($"Folder/File:         {file}");
-                    Logger.WriteLine($"Hash Algorithm:      {algorithm}");
-                    Logger.WriteLine($"Threads:             {threads}");
-                    Logger.WriteLine("--------------------------------------------------------------------------------");
-
-                    PathInfo path = new PathInfo(file, checksumFile, exclusions);
-                    Stopwatch watch = new Stopwatch();
-                    watch.Start();
-                    path.Crawl(!(bool)excludeSubDir);
-                    if (path.Files != null)
-                    {
-                        Logger.WriteLine($"File path: {path.FullPath}");
-                        path.Check((HashAlgorithm)algorithm, (int)threads);
-                        watch.Stop();
-
-                        Logger.WriteLine("--------------------------------------------------------------------------------");
-                        Logger.WriteLine($"Folders:             {path.DirectoryCount}");
-                        Logger.WriteLine($"Files:               {path.FileCount}");
-                        Logger.WriteLine($"Time (ms):           {watch.ElapsedMilliseconds}");
-                        Logger.WriteLine("--------------------------------------------------------------------------------");
-                    }
-
-                    // If settings were specified, then send the notifications
-                    if (settings != null)
-                    {
-                        settings.Send();
-                    }
-
-                    return SUCCESS;
+                    return GetChecksums(
+                        file,
+                        checksumFile,
+                        (HashAlgorithm)algorithm,
+                        (int)threads,
+                        (bool)excludeSubDir,
+                        settingsFile);
                 }
                 else
                 {
-                    if (!PathInfo.IsFile(file))
-                    {
-                        Logger.WriteLine($"The file '{file}' is not a valid file.");
-                        return ERROR_NOT_FILE;
-                    }
-
-                    string? fileHash = HashInfo.GetFileHash(file, (HashAlgorithm)algorithm);
-                    if (string.IsNullOrWhiteSpace(fileHash))
-                    {
-                        Logger.WriteLine($"The hash for file '{file}' could not be generated.");
-                        return ERROR_NO_HASH;
-                    }
-
-                    // If the hash only option was specified, then just display
-                    // the hash of the file
-                    if (hashOnlyOption)
-                    {
-                        Logger.WriteLine(fileHash);
-                        return SUCCESS;
-                    }
-
-                    // The the hash option was specified, compare the file hash
-                    // with the hash passed through the argument
-                    if (!string.IsNullOrWhiteSpace(hashOption))
-                    {
-                        //int returnValue = string.Compare(fileHash, hashOption, true) == 0 ? SUCCESS : ERROR_HASH_NOT_MATCH;
-
-                        if (fileHash.Equals(hashOption, StringComparison.OrdinalIgnoreCase))
-                        {
-                            Logger.WriteLine($"The file hash matches the hash '{hashOption}'");
-                            return SUCCESS;
-                        }
-                        else
-                        {
-                            Logger.WriteLine($"The file hash '{fileHash}' does not match the hash '{hashOption}'");
-                            return ERROR_HASH_NOT_MATCH;
-                        }
-                    }                    
+                    return GetFileChecksum(
+                        file,
+                        (HashAlgorithm)algorithm,
+                        hashOnlyOption,
+                        hashOption);
                 }
-
-                return SUCCESS;
             }
             catch (Exception ex)
             {
                 Logger.WriteLine($"An error occurred. Error: {ex.Message}");
                 return ERROR;
             }
+        }
+
+        /// <summary>
+        /// Gets the checksums for all files in a folder.
+        /// </summary>
+        /// <param name="file">
+        /// The file/folder used to generate the checksums.
+        /// </param>
+        /// <param name="checksumFile">
+        /// The checksum file name.
+        /// </param>
+        /// <param name="algorithm">
+        /// The hash algorithm used to generate the checksums.
+        /// </param>
+        /// <param name="threads">
+        /// The number of threads used to generate the checksums.
+        /// </param>
+        /// <param name="excludeSubDir">
+        /// Flag indicating whether subfolders are to be crawled.
+        /// </param>
+        /// <param name="settingsFile">
+        /// The name of the settings configuration file.
+        /// </param>
+        /// <returns>
+        /// 0 on success, otherwise a non-zero value.
+        /// </returns>
+        private static int GetChecksums(
+            string file,
+            string checksumFile,
+            HashAlgorithm algorithm,
+            int threads,
+            bool excludeSubDir,
+            string? settingsFile)
+        {
+            // Read the settings file if one was provided as an argument
+            Settings? settings = null;
+            Exclusions? exclusions = null;
+            if (!string.IsNullOrWhiteSpace(settingsFile))
+            {
+                ISettingsFile xmlFile = new XmlFile(settingsFile);
+                settings = xmlFile.Read();
+                if (settings != null)
+                {
+                    exclusions = settings.Exclusions;
+                }
+            }
+
+            Logger.WriteLine("--------------------------------------------------------------------------------");
+            Logger.WriteLine($"Folder/File:         {file}");
+            Logger.WriteLine($"Hash Algorithm:      {algorithm}");
+            Logger.WriteLine($"Threads:             {threads}");
+            Logger.WriteLine("--------------------------------------------------------------------------------");
+
+            PathInfo path = new PathInfo(file, checksumFile, exclusions);
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            path.Crawl(!(bool)excludeSubDir);
+            if (path.Files != null)
+            {
+                Logger.WriteLine($"File path: {path.FullPath}");
+                path.Check((HashAlgorithm)algorithm, (int)threads);
+                watch.Stop();
+
+                Logger.WriteLine("--------------------------------------------------------------------------------");
+                Logger.WriteLine($"Folders:             {path.DirectoryCount}");
+                Logger.WriteLine($"Files:               {path.FileCount}");
+                Logger.WriteLine($"Time (ms):           {watch.ElapsedMilliseconds}");
+                Logger.WriteLine("--------------------------------------------------------------------------------");
+            }
+
+            // If settings were specified, then send the notifications
+            if (settings != null)
+            {
+                settings.Send();
+            }
+
+            return SUCCESS;
+        }
+
+        /// <summary>
+        /// Gets the checksum for a single file.
+        /// </summary>
+        /// <param name="file">
+        /// The full path to the file.
+        /// </param>
+        /// <param name="algorithm">
+        /// The hash algorithm used to generate the checksum.
+        /// </param>
+        /// <param name="hashOnlyOption">
+        /// Flag indicating that the checksum of the file is to be generated
+        /// and then displayed.
+        /// </param>
+        /// <param name="providedHash">
+        /// The hash value to validate against the hash of the file.
+        /// </param>
+        /// <returns>
+        /// 0 on success, otherwise a non-zero value.
+        /// </returns>
+        private static int GetFileChecksum(string file, HashAlgorithm algorithm, bool hashOnlyOption, string providedHash)
+        {
+            if (!PathInfo.IsFile(file))
+            {
+                Logger.WriteLine($"The file '{file}' is not a valid file.");
+                return ERROR_NOT_FILE;
+            }
+
+            string? fileHash = HashInfo.GetFileHash(file, algorithm);
+            if (string.IsNullOrWhiteSpace(fileHash))
+            {
+                Logger.WriteLine($"The hash for file '{file}' could not be generated.");
+                return ERROR_NO_HASH;
+            }
+
+            // If the hash only option was specified, then just display
+            // the hash of the file
+            if (hashOnlyOption)
+            {
+                Logger.WriteLine(fileHash);
+                return SUCCESS;
+            }
+
+            // The the hash option was specified, compare the file hash
+            // with the hash passed through the argument
+            if (!string.IsNullOrWhiteSpace(providedHash))
+            {
+                if (fileHash.Equals(providedHash, StringComparison.OrdinalIgnoreCase))
+                {
+                    Logger.WriteLine($"The file hash matches the hash '{providedHash}'");
+                    return SUCCESS;
+                }
+                else
+                {
+                    Logger.WriteLine($"The file hash '{fileHash}' does not match the hash '{providedHash}'.");
+                    return ERROR_HASH_NOT_MATCH;
+                }
+            }
+
+            return SUCCESS;
         }
     }
 }
