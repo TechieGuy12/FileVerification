37ce1923d5d48a4cd7bca29c28b6e88f04f737b5
TechieGuy12 <psalmon12@gmail.com>
2024-11-08 19:19:03 -0500

    Add remove file  argument and added complex type for arguments

---
+++ FileVerification/Arguments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE.FileVerification
+{
+    public class Arguments
+    {
+
+        public string? File { get; set; }
+        
+        public string? ChecksumFile { get; set; }
+        
+        public bool ExcludeSubDir { get; set; }
+
+        public HashAlgorithm Algorithm { get; set; }
+
+        public string? Hash {  get; set; }  
+
+        public bool HashOnly { get; set; }
+        
+        public int? Threads { get; set; }
+
+        public string? SettingsFile { get; set; }
+
+        public bool RemoveFile { get; set; }
+    }
+}
